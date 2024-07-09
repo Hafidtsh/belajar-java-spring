9425cfd0daaf6787a84e7f118d05035bb04b3e4c
devziad2021 <devziad2021@gmail.com>
2024-07-09 10:17:46 +0700

    Your commit message

---
+++ src/test/java/belajar/java/spring/demo/BelajarJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package belajar.java.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
